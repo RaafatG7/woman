c984f7baa416a1e2fb2041fdf9cb23ecc7f565ef
RaafatG7 <raafatmohamed13112012@gmail.com>
2024-12-30 22:36:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,170 @@
+list = ['farida', 'mahdy', "ra'fat", 'salah', 'gamal', 'islam', 'hazem']
+
+for(names in list){
+    document.write('<br /> compatitors = ', list[names])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list = ['farida','islam','mahdi','salah','gamal','hazem']
+
+for (names in list){
+    document.write('<br / compatitors= ',list [names])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+list  = [1,2,3,4,5,6,7,8,]
+i = 0
+
+while(i < list.lenth) {
+  document.write( '<br / compatitors =',list[i]);
+  i++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function calc (num1,num2,op){
+switch ("-"){
+case "+":
+  console.log(num1,num2,op)
+  break
+case "-":
+  console.log(num1,num2,op)
+  break
+  default:console.error('error')
+}
+}
+calc(num1,num2,op)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function comp( x ){
+
+for(names in x){
+
+   console.log(x[names])
+}
+}
+comp(['farida','salah','raaft'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function ula ( x ){
+  if (x%2===0){
+    console.log ('even')
+  }
+else{
+  console.log('odd')
+} 
+}
+ula(9)
+
+
+
+
+
+css:bootstrap
+
+
+javaScript:jQuery
+
+
+git:hacking   cmd
+
+
+
+
+portfolio
